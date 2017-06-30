c5cf45c47f65a0720db2e77b6357a426267bda48
cmhedrick <christopher.m.hedrick@gmail.com>
2017-06-29 23:25:14 -0400

    DAY 25/100 THATS 1/4TH OF 100!

--- metador.py
+++ metador.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python3
 import piexif
 
-exif_dict = piexif.load("IMG.JPG")
-for tag_type in ("0th", "Exif", "GPS", "1st"):
-    for tag in exif_dict[tag_type]:
-        print(piexif.TAGS[tag_type][tag]["name"], exif_dict[tag_type][tag])
+if __name__ == "__main__":
+    print('welcome:')
+    in_file = input('File Path==> ')
+    cmd = input('command==> ')
 
-piexif.remove("IMG.JPG")
-print('\nREMOVED?\n')
-for tag_type in ("0th", "Exif", "GPS", "1st"):
-    for tag in exif_dict[tag_type]:
-        print(piexif.TAGS[tag_type][tag]["name"], exif_dict[tag_type][tag])
+    if cmd.lower() == 'remove':
+        print('REMOVING DATA')
+        piexif.remove(in_file)
+
+    elif cmd.lower() == 'read':
+        print('READING...')
+        exif_dict = piexif.load(in_file)
+        for tag_type in ("0th", "Exif", "GPS", "1st"):
+            for tag in exif_dict[tag_type]:
+                print(piexif.TAGS[tag_type][tag]["name"],
+                      exif_dict[tag_type][tag])
