c9cc9e9a9a3b438ff98c687114e91f4e2dfc44a8
cmhedrick <christopher.m.hedrick@gmail.com>
2017-06-29 23:35:46 -0400

    DAY 26/100 minor touch ups :p

--- metador.py
+++ metador.py
@@ -2,7 +2,9 @@
 import piexif
 
 if __name__ == "__main__":
-    print('welcome:')
+    print('welcome! cmds include:')
+    print('remove| removes data')
+    print('read| print tags')
     in_file = input('File Path==> ')
     cmd = input('command==> ')
 
@@ -11,9 +13,12 @@ if __name__ == "__main__":
         piexif.remove(in_file)
 
     elif cmd.lower() == 'read':
-        print('READING...')
-        exif_dict = piexif.load(in_file)
-        for tag_type in ("0th", "Exif", "GPS", "1st"):
-            for tag in exif_dict[tag_type]:
-                print(piexif.TAGS[tag_type][tag]["name"],
-                      exif_dict[tag_type][tag])
+        try:
+            print('READING...')
+            exif_dict = piexif.load(in_file)
+            for tag_type in ("0th", "Exif", "GPS", "1st"):
+                for tag in exif_dict[tag_type]:
+                    print(piexif.TAGS[tag_type][tag]["name"],
+                          exif_dict[tag_type][tag])
+        except:
+            print('POSSIBLY NO TAGS!?')
